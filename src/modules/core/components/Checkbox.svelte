e5b377d9779722f5f887354f1a181b954be6dbe4
Jonathan Rourke <jonpng11@gmail.com>
2023-01-25 02:19:17 -0500

    feat(core): add theme styles to Checkbox

--- src/modules/core/components/Checkbox.svelte
+++ src/modules/core/components/Checkbox.svelte
@@ -1,57 +1,89 @@
 <script lang="ts">
   export let value = '';
   export let className = '';
+  export let checked = false;
+  export let disabled = false;
   export let onChange = () => {};
+  const classNames = (...classNames) => {
+    return [className, ...classNames].join(' ');
+  };
 </script>
 
-<div class={className}>
-  <label for={value}><slot /></label>
+<div class={classNames('checkbox', className)}>
   <input
+    type="checkbox"
     id={value}
     name={value}
-    type="checkbox"
+    checked={checked}
+    disabled={disabled}
     on:change={onChange}
   />
+  <label for={value}>
+    <span class="check"></span>
+    <slot />
+  </label>
 </div>
 
 <style lang="scss">
-  .checkbox {
-    background: white;
-    display: block;
-    font-size: 10vmin;
-    width: 1em;
-    height: 1em;
-    border: solid 2px grey;
-    position: relative;
-    cursor: pointer;
-    > input {
-      opacity: 0;
-      position: absolute;
-    }
-  }
-
-  .check {
+  input {
+    opacity: 0;
     position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    margin: -3px;
-    clip-path: polygon(0 0, 50% 0, 100% 0, 100% 100%, 50% 100%, 0 100%);
-
-    border: solid 4px hotpink;
-
-    transition: 0.4s cubic-bezier(0.8, 0, 0.2, 1);
-    transition-property: background-color, clip-path, border-color;
-    transition-delay: 0.1s, 0s;
-
-    input:checked + & {
-      // clip-path: polygon(41% 52%, 26% 35%, 10% 51%, 37% 88%, 37% 88%, 37% 88%, 37% 88%, 37% 88%, 91% 33%, 78% 18%);
-      background-color: hotpink;
-      border-color: transparent;
+  }
+  label {
+    position: relative;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    padding: calc(var(--spacingBase) / 4) var(--spacingBase);
+  }
+  .check {
+    position: relative;
+    background: white;
+    border: calc(var(--border-size) * 2) solid var(--input--border-color);
+    margin: {
+      right: var(--spacingBase);
+    }
+    input:checked + label & {
+      border-color: var(--input--success);
       transition-delay: 0s;
-      //clip-path: polygon(28% 38%, 41% 53%, 75% 24%, 86% 38%, 40% 78%, 15% 50%);
-      clip-path: polygon(0 0, 100% 0, 100% 50%, 37.5% 50%, 37.5% 100%, 0 100%);
+    }
+    input:disabled + label & {
+      background-color: var(--background--disabled);
+      border-color: var(--input--disabled);
+    }
+    &, &:before {
+      display: block;
+      width: 1em;
+      height: 1em;
+      transition: 0.3s cubic-bezier(0.8, 0, 0.2, 1);
+    }
+    &:before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      z-index: 10;
+      //margin: calc(var(--border-size) * -4) calc(var(--border-size) * -2);
+      clip-path: polygon(0 0, 50% 0, 100% 0, 100% 100%, 50% 100%, 0 100%);
+      transition-property: background-color, clip-path, border-color, transform;
+      transition-delay: 0.1s, 0s;
+      transform: scale(1) translate(-2px, -2px);
+      input:checked + label &, input:checked + label & {
+        transition-delay: 0s;
+        border-color: transparent;
+        clip-path: polygon(0 0, 100% 0, 100% 32.5%, 37.5% 32.5%, 37.5% 70%, 0 70%);
+        transform: scale(0.6) scaleY(-1) rotate(45deg) translate(1px, 8px);
+        transform-origin: center;
+      }
+      input:checked + label & {
+        background-color: var(--input--success);
+      }
+      input:disabled + label & {
+        background-color: var(--background--disabled);
+      }
     }
   }
 </style>
