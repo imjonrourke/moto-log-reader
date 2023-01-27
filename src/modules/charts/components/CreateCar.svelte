1a9826bd989646775b5ff9f11de4ad907bcb9490
Jonathan Rourke <jonpng11@gmail.com>
2023-01-27 01:07:51 -0500

    feat(charts): create component for selecting car

---
+++ src/modules/charts/components/CreateCar.svelte
@@ -0,0 +1,33 @@
+<script>
+
+</script>
+<div class="car">
+  <p>Choose your car</p>
+  <div class="car__year">
+    <label for="year">Choose year</label>
+    <select name="year" id="year">
+      <option value="default">Choose year...</option>
+      <option value="2023">2023</option>
+      <option value="2022">2022</option>
+      <option value="2021">2021</option>
+    </select>
+  </div>
+  <div class="car__make">
+    <label for="make">Choose make</label>
+    <select name="make" id="make">
+      <option value="default">Choose make...</option>
+      <option value="audi">Audi</option>
+      <option value="bmw">BMW</option>
+      <option value="chevrolet">Chevrolet</option>
+    </select>
+  </div>
+  <div class="car__model">
+    <select name="model" id="model">
+      <option value="default">Choose model...</option>
+      <option value="a3">A3</option>
+      <option value="a4">A4</option>
+      <option value="a5">A5</option>
+    </select>
+  </div>
+</div>
+<style lang="scss"></style>
